e281799e51598bb658e216fbd180a4ac1b689e65
Cesar Andreu <cesarandreu@gmail.com>
2013-03-31 16:46:49 -0400

    Fixed previous commit and updated the README with an accurate description and links.
    Signed-off-by: Cesar Andreu <cesarandreu@gmail.com>

--- public/js/movementJS.js
+++ public/js/movementJS.js
@@ -1,4 +1,6 @@
-document.write("Hello");
+
+
+	document.write("Hello");
 			
 			var host = "http://"+window.location.hostname;
 			var socket = io.connect(host);
@@ -7,9 +9,48 @@ document.write("Hello");
 			
 			document.onkeydown = keydown;
 			document.onkeyup = keyup;
-			
-			
-			function keydown(e)
+
+
+        $('#up').bind('touchstart mousedown', function(){
+        	console.log('TOUCHSTART!');
+        	document.body.style.backgroundColor = '#00FF00';
+ 			socket.emit('movementUp', {data: true, player: id.charAt(id.length-1)}, function(position)
+ 						{	
+ 							console.log(position);
+ 						});        
+ 		});
+        $('#down').bind('touchstart mousedown', function(){
+        	console.log('TOUCHEND!');
+        	document.body.style.backgroundColor = '#FF0000';
+ 			socket.emit('releasedUp', {data: true, player: id.charAt(id.length-1)}, function(position)
+ 						{	
+ 							console.log(position);
+ 						});
+
+        });	
+
+        $('#up').bind('touchnend mouseup', function(){
+        	console.log('TOUCHSTART!');
+        	document.body.style.backgroundColor = '#00FF00';
+ 			socket.emit('movementUp', {data: true, player: id.charAt(id.length-1)}, function(position)
+ 						{	
+ 							console.log(position);
+ 						});        
+ 		});
+        $('#down').bind('touchend mouseup', function(){
+        	console.log('TOUCHEND!');
+        	document.body.style.backgroundColor = '#FF0000';
+ 			socket.emit('releasedUp', {data: true, player: id.charAt(id.length-1)}, function(position)
+ 						{	
+ 							console.log(position);
+ 						});
+
+        });	
+
+
+		
+
+        function keydown(e)
 			{	
 				var key = window.event || e;
 				var keyCode = key.charCode || e.keyCode;
@@ -61,3 +102,6 @@ document.write("Hello");
  				
  				}
  			}
+
+
+
